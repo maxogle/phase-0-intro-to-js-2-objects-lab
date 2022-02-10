7bed501f9546a3a3ca6e532b3aa0cb474f03b2c5
alexandermaxwellogle <maxogle@gmail.com>
2022-02-10 17:06:29 -0500

    commit tree 7119b06823e1440508f81b8fc538690cc051e06f

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway" 
+}
+
+beforeEach(function () {
+    for (const key in employee) {
+      delete employee['Sam'];
+    }
+
+    employee.name = 'Sam';
+  });
+
+  function updateEmployeeWithKeyAndValue(object, key, value) {
+    const updatedEmployee = { ...employee };
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+  }
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+      employee.streetAddress = '12 Broadway';
+      return employee
+  }
+
+  function deleteFromEmployeeByKey(employee, key) {
+      const newEmployee = { ...employee };
+      delete newEmployee.name;
+      return newEmployee
+
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, name) {
+      delete employee.name;
+      return employee
+
+  }
+
+       
